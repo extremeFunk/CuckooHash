8a6123d9bb872b79bdd4d362b0c45f7658ff5f57
roy <roy.barak.m@gmail.com>
2019-10-26 18:02:20 +0200

    Generic added

--- src/main/java/io/rainrobot/cuckoohash/CuckooHash.java
+++ src/main/java/io/rainrobot/cuckoohash/CuckooHash.java
@@ -1,8 +1,8 @@
 package io.rainrobot.cuckoohash;
 
-public class CuckooHash {
+public class CuckooHash <T extends Object>{
 
-    private Object[] array;
+    private T[] array;
     private LoopProtectService loopProtect;
     private IHashGenerator hashGenerator;
 
@@ -11,23 +11,23 @@ public class CuckooHash {
                         LoopProtectService loopProtect) {
         this.hashGenerator = hashGenerator;
         this.loopProtect = loopProtect;
-        this.array = new Object [initialSize];
+        this.array = (T[])new Object[initialSize];
     }
 
-    public void put(Object val) {
+    public void put(T val) {
             //generate hashes
         Integer[] hashList = hashGenerator.getHash(val, array.length);
             //check if a key available (resize if in loop)
         if (isKeyAvailable(val, hashList)) return;
         else {
             //replace item at first position
-            Object replaced = array[hashList[0]];
+            T replaced = array[hashList[0]];
             array[hashList[0]] = val;
             put(replaced);
         }
     }
 
-    private boolean isKeyAvailable(Object val, Integer[] hashList) {
+    private boolean isKeyAvailable(T val, Integer[] hashList) {
         for(Integer hash : hashList) {
             loopProtect.checkIfLoopAndResize(this, hash);
             if (array[hash] == null || array[hash].equals(val)) {
@@ -39,7 +39,7 @@ public class CuckooHash {
         return false;
     }
 
-    public boolean remove(Object val) {
+    public boolean remove(T val) {
         Integer[] hashList = hashGenerator.getHash(val, array.length);
         for(Integer hash : hashList) {
             if (array[hash] == val) {
@@ -50,7 +50,7 @@ public class CuckooHash {
         return false;
     }
 
-    public boolean exist(Object val) {
+    public boolean exist(T val) {
         Integer[] hashList = hashGenerator.getHash(val, array.length);
         for(Integer hash : hashList) {
             if (array[hash] == val) {
@@ -63,23 +63,23 @@ public class CuckooHash {
     public void reSizeArray() {
         //copy current array ref
         int x2size = array.length * 2;
-        Object[] oldArray = array;
+        T[] oldArray = array;
 
         //create new array from old values
-        array = new Object[x2size];
-        for(Object val : oldArray) {
+        array = (T[])new Object[x2size];
+        for(T val : oldArray) {
             if(val != null) {
                 put(val);
             }
         }
     }
 
-    public Object get(int key) {
+    public T get(int key) {
         return array[key];
     }
 
 
-    public Object[] getArray() {
+    public T[] getArray() {
         return array;
     }
 
